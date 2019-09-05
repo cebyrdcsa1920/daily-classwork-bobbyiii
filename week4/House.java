5535b3395b904a675927b6f4402616a4df4fcb71
bobbyiii <bobbysimpson674@gmail.com>
2019-09-05 14:17:53 +0000

    Add house to week4

---
+++ week4/House.java
@@ -0,0 +1,17 @@
+import java.awt.Color;
+
+public class House
+{
+    private String model;
+    private Color color;
+    private int numRooms
+    private double numBathRooms
+
+    public House()
+    {
+        model; one story house
+        color = Color.BLUE;
+        numRooms = 3;
+        numBathRooms = 2.5
+    }
+}
