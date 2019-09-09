001ee2c8c8b09fc3ac5c8ef18004106364c9cfd3
bobbyiii <bobbysimpson674@gmail.com>
2019-09-09 14:04:52 +0000

    Add javadocs to Car.java

--- week4/Car.java
+++ week4/Car.java
@@ -13,5 +13,31 @@ public class Car
         color = Color.YELLOW;
        numPassengers = 2;
          amountOfGas= 20.5;
+         mpg = 15.0;
+    }
+
+    /**
+     * Drives the car the given distance. Subtracts gas from tank.
+     *
+     * @param distance. The distance driven in miles
+     */
+        public void drive(double distance)
+        {
+            amountOfGas = distance / mpg
+
+        }
+        /**
+         * get the amount of gas in the tank
+         *
+         * @return amount of gas
+         */
+
+        public double getAmountOfGas()
+        {
+            return amountOfGas()
+    }
+    public String toString()
+    {
+        return "This is my Mustang"
     }
 }
